292511da7e02b033c5d43f2d764cf4c92562043b
NIkita <kiton1994@gmail.com>
2017-10-16 12:25:40 +0300

    Добавлена обработка папок Добавлена настройка параметров через командную строку

--- watermark.py
+++ watermark.py
@@ -1,5 +1,7 @@
+from tqdm import tqdm
 import argparse
 import math
+import os
 
 from PIL import Image, ImageDraw, ImageFont, TiffImagePlugin
 
@@ -10,8 +12,21 @@ OPACITY_COEF = 2.55
 WHITE = 255
 FULL_TRANSPARENT = 0
 
+PNG = "PNG"
+JPG = "JPG"
+JPEG = "JPEG"
+TIFF = "TIFF"
+TIF = "TIF"
+ALLOWED_FORMATS = tuple(item.lower() for item in (PNG, JPEG, JPG, TIFF, TIF))
+
+
 
 class UnsupportedFileExtension(Exception):
+
+    pass
+
+
+class DirectoryMatching(Exception):
 
     pass
 
@@ -40,7 +55,7 @@ def cyclic_shift(string, n_chars=10, offset=0):
 
 
 def watermark_image(image_obj, text, font_file, angle=45,
-                    text_opacity=20, fill_color=200, fontsize=50,
+                    text_opacity=20, fill_color=220, fontsize=50,
                     padding=200, space_interval=10):
 
     image_obj = image_obj.convert("RGBA")
@@ -94,7 +109,7 @@ def watermark_file(input_file, output_file, text, font_file, **kwargs):
     quality = kwargs.pop("quality")
     compress = kwargs.pop("compress")
 
-    if original.format == "TIFF":
+    if original.format == TIFF:
         with TiffImagePlugin.AppendingTiffWriter(output_file, True) as tif:
             for i in range(original.n_frames):
                 original.seek(i)
@@ -105,12 +120,45 @@ def watermark_file(input_file, output_file, text, font_file, **kwargs):
 
                 out.save(tif, compression=compression, quality=quality)
                 tif.newFrame()
-    elif original.format in ("PNG", "JPG", "JPEG"):
+    elif original.format in (PNG, JPG, JPEG):
         out = watermark_image(original, text, font_file, **kwargs)
         out.save(output_file, quality=quality)
     else:
         raise UnsupportedFileExtension(
             "Format {0} not supported".format(original.format))
+
+
+def replace_two_slash(name):
+
+    return name.replace(os.path.sep + os.path.sep, os.path.sep)
+
+
+def find_images_recursively(folder):
+
+    images = []
+
+    for directory, dirs, files in os.walk(folder):
+        for filename in files:
+            name, ext = os.path.splitext(filename)
+            if ext[1:].lower() in ALLOWED_FORMATS:
+                filename = os.path.join(directory, filename)
+                images.append(filename)
+
+    return images
+
+
+def create_output_file(filename, input_folder, output_folder):
+
+    input_folder = input_folder.rstrip(os.path.sep)
+    filename = filename.replace(input_folder, "").strip(os.path.sep)
+
+    target = os.path.join(output_folder, filename)
+
+    # Воссоздадим иерархию папок как в источнике
+    dest_folder, _ = os.path.split(target)
+    os.makedirs(dest_folder, exist_ok=True)
+
+    return target
 
 
 def main():
@@ -139,16 +187,55 @@ def main():
     parser.add_argument("--compress",
                         help="Whether enable JPEG compression. For TIFF only",
                         action="store_true")
+    parser.add_argument("--angle", help="Text angle", type=int, default=45)
+    parser.add_argument("--text_opacity", help="Text opacity", type=int, default=20)
+    parser.add_argument("--fill_color", help="Fill color. RGB digit", type=int, default=220)
+    parser.add_argument("--fontsize", help="Font size", type=int, default=50)
+    parser.add_argument("--padding", help="Text padding", type=int, default=200)
+    parser.add_argument("--space_interval", help="Text interval", type=int, default=10)
 
     args = parser.parse_args()
 
-    watermark_file(
-        args.input,
-        args.output,
-        args.text,
-        args.font,
-        quality=args.quality,
-        compress=args.compress)
+    args.input = replace_two_slash(args.input)
+    args.output = replace_two_slash(args.output)
+
+    if os.path.isdir(args.input):
+        if not os.path.isdir(args.output):
+            raise DirectoryMatching("If input is a directory output MUST be a directory")
+
+        images_to_watermark = find_images_recursively(args.input)
+
+        if input("There are {0} images to watermark. " \
+                 "Do you want to continue?(y|N): ".format(
+                     len(images_to_watermark))).lower() == "y":
+            for i in tqdm(range(len(images_to_watermark))):
+                watermark_file(
+                    images_to_watermark[i],
+                    create_output_file(images_to_watermark[i], args.input, args.output),
+                    args.text,
+                    args.font,
+                    quality=args.quality,
+                    compress=args.compress,
+                    angle=args.angle,
+                    text_opacity=args.text_opacity,
+                    fill_color=args.fill_color,
+                    fontsize=args.fontsize,
+                    padding=args.padding,
+                    space_interval=args.space_interval)
+    else:
+        watermark_file(
+            args.input,
+            args.output,
+            args.text,
+            args.font,
+            quality=args.quality,
+            compress=args.compress,
+            angle=args.angle,
+            text_opacity=args.text_opacity,
+            fill_color=args.fill_color,
+            fontsize=args.fontsize,
+            padding=args.padding,
+            space_interval=args.space_interval)
 
 
 if __name__ == "__main__":
